5437d9551a687f62716d3d98f3eb434e166225d5
VizualM <maxime.epinette@y-nov.com>
2015-03-04 12:22:12 +0100

    Mise en place d'unepremier carré en Polyline, sur la fin de galerie ingesup

--- app/src/main/java/vizual/testapiv2/MapsActivity.java
+++ app/src/main/java/vizual/testapiv2/MapsActivity.java
@@ -10,6 +10,8 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import vizual.geolocation.GPSManager;
 import vizual.geolocation.LocationManager;
@@ -93,12 +95,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         CameraPosition cameraPosition = CameraPosition.builder()
                 .target(mapCenter)
-                .zoom(100)
+                .zoom(16)
                 .bearing(0)
                 .build();
 
         // Animate the change in camera view over 2 seconds
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),
                 500, null);
+
+        PolylineOptions rectOptions = new PolylineOptions()
+                .add(new LatLng(44.855157, -0.567730))
+                .add(new LatLng(44.855286, -0.567542))  // North of the previous point, but at the same longitude
+                .add(new LatLng(44.855157, -0.567392))  // Same latitude, and 30km to the west
+                .add(new LatLng(44.855031, -0.567574))  // Same longitude, and 16km to the south
+                .add(new LatLng(44.855157, -0.567730)); // Closes the polyline.
+         Polyline polyline = mMap.addPolyline(rectOptions);
+        //Affichage du premier carré !
     }
 }
